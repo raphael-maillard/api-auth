32509d75e8ea48fda8125f87fa024bf0e33e4372
raphael-maillard <raphael.maillard@gmail.com>
2021-01-20 13:42:14 +0100

    install mailer - verifierMail and google-mailer

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -97,6 +97,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
         }
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
+        return new RedirectResponse($this->urlGenerator->generate('/'));
         throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
